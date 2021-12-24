ef3ded5f966f51d99fe461cbb63475de50be1225
HGB467 <67868309+HGB467@users.noreply.github.com>
2021-12-24 21:00:05 +0500

    hal

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ io.on('connection',socket=>{
 })
 
 
-server.listen(process.env.PORT || 3000,()=>{
+server.listen(process.env.PORT || 5000,()=>{
     console.log("App Listening Successfully!")
 })
 
