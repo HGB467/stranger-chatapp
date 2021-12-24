b2ebcf99116d2e01656d6033973db2206199cbbd
HGB467 <67868309+HGB467@users.noreply.github.com>
2021-12-24 15:31:45 +0500

    make it better

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const io = require("socket.io")(server)
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/public/index.html')
+    res.sendFile('index.html')
 })
 
 let connectedUsers = [];
