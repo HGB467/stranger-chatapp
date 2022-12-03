c18f83b011a20dd69de83cb7daa3b86f0fb16446
HGB467 <67868309+HGB467@users.noreply.github.com>
2022-12-03 16:56:08 +0500

    modified

--- public/main.js
+++ public/main.js
@@ -162,8 +162,13 @@ function peerConnection(){
          const channel = e.channel;
    
     channel.onmessage=(e)=>{
+        console.log(e.data)
        appendMessages(false,e.data)
        const ha = document.getElementById('msgCont')
+       const a = document.createElement('a');
+       a.href="#nam";
+       a.click();
+       a.remove();
        if(ha.scrollTop>=ha.scrollHeight-880){
         ha.scrollTop = ha.scrollHeight;
         if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
